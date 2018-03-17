6e530c96c7cfd64d86c495edd52292155181e729
frkodes <frkalderon@gmail.com>
2018-03-16 18:38:40 -0600

    Adding the IG token as env var

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,7 +9,7 @@ class PagesController extends Controller
 {
     public function index()
     {
-    	$instagram = new Instagram('27157690.1677ed0.b85457663f9b44d7a1000c5f6fb7a8be');
+    	$instagram = new Instagram(env('IG-TOKEN'));
     	$posts = array_slice($instagram->get(), 0, 3);
     	return View('pages.index', compact('posts'));
     }
